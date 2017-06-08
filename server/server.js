a2a42eec3d3d40f481e07e3e8021814555ae25a4
samps01 <samps01@gmail.com>
2017-06-08 02:27:52 -0400

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -16,14 +16,13 @@ app.use(express.static(publicPath));
 io.on('connection',(socket)=>{
    console.log('New user');
 
-   socket.emit('newMessage',{
-      from:'Sam',
-       text:'Text',
-       created:Date()
-   });
-
    socket.on('createMessage',(message)=>{
       console.log(message);
+      io.emit('newMessage',{
+         from:message.from,
+          text:message.text,
+          createdAt: new Date()
+      })
    });
    socket.on('disconnect',()=>{
       console.log('Client disconnected');
