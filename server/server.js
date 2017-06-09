940247f23b55446cc201db02187eb32c6b956e5f
samps01 <samps01@gmail.com>
2017-06-09 18:00:10 -0400

    Send message to only people in same room

--- server/server.js
+++ server/server.js
@@ -36,13 +36,18 @@ io.on('connection',(socket)=>{
         callback()
     });
     socket.on('createMessage',(message,callback)=>{
-      console.log(message);
-      io.emit('newMessage',generateMessage(message.from,message.text));
+        let user = users.getUser(socket.id);
+        if(user && isRealString(message.text)){
+            io.to(user.room).emit('newMessage',generateMessage(user.name,message.text));
+        }
       callback();
    });
 
     socket.on('createLocationMessage',(location)=>{
-        io.emit('newLocationMessage',generateLocationMessage('User',location.latitude,location.longitude));
+        let user = users.getUser(socket.id);
+        if(user){
+            io.to(user.room).emit('newLocationMessage',generateLocationMessage(user.name,location.latitude,location.longitude));
+        }
     });
    socket.on('disconnect',()=>{
        let user = users.removeUser(socket.id);
