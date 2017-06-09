1ecfd754738702fe0c64006b10724ee51b91499c
samps01 <samps01@gmail.com>
2017-06-09 13:18:02 -0400

    Add format to messages using Mustache

--- public/js/index.js
+++ public/js/index.js
@@ -9,19 +9,27 @@ socket.on('disconnect',function(){
 
 socket.on('newMessage',function(message){
     let formattedTime = moment(message.createdAt).format('hh:mm a');
-    let li = $('<li></li>');
-    li.text(`${message.from} ${formattedTime} : ${message.text}`);
-    $('#messages').append(li);
+    let template = $('#message-template').html();
+    let html = Mustache.render(template,{
+        text:message.text,
+        from:message.from,
+        createdAt:formattedTime
+    });
+
+    $('#messages').append(html);
 });
 
 socket.on('newLocationMessage',function (message) {
+
     let formattedTime = moment(message.createdAt).format('hh:mm a');
-    let li = $('<li></li>');
-    let a = $('<a target="_blank">My Current Location</a>')
-    li.text(`${message.from} ${formattedTime}: `);
-    a.attr('href',message.url);
-    li.append(a);
-    $('#messages').append(li);
+    let template  = $('#location_message-template').html();
+    let html = Mustache.render(template,{
+        url:message.url,
+        from:message.from,
+        createdAt:formattedTime
+    });
+
+    $('#messages').append(html);
 });
 
 
