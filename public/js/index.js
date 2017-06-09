18df8cae183e9c64dade2bd18c0c23b8ccb2f5fd
samps01 <samps01@gmail.com>
2017-06-09 13:49:25 -0400

    Add autoscrolling

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,18 @@
 const socket = io();
+
+function scrollToBottom() {
+    let messages = $('#messages');
+    let newMessage = messages.children('li:last-child')
+    let clientHeight = messages.prop('clientHeight');
+    let scrollTop = messages.prop('scrollTop');
+    let scrollHeight = messages.prop('scrollHeight');
+    let newMessageHeight = newMessage.innerHeight();
+    let lastMessageHeight = newMessage.prev().innerHeight();
+    if(clientHeight+scrollTop+newMessageHeight+lastMessageHeight >= scrollHeight){
+        messages.scrollTop(scrollHeight);
+    }
+}
+
 socket.on('connect',function(){
     console.log('Connected to server');
 });
@@ -17,6 +31,7 @@ socket.on('newMessage',function(message){
     });
 
     $('#messages').append(html);
+    scrollToBottom();
 });
 
 socket.on('newLocationMessage',function (message) {
@@ -30,6 +45,7 @@ socket.on('newLocationMessage',function (message) {
     });
 
     $('#messages').append(html);
+    scrollToBottom();
 });
 
 
